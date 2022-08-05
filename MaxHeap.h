789c1c1a1d8337f5658bea18b56e948d233a439d
Stancugeo <96280343+Stancugeo@users.noreply.github.com>
2022-08-05 17:59:34 +0300

    MaxHeap class implementation in c++

---
+++ MaxHeap.h
@@ -0,0 +1,27 @@
+#include <iostream>
+using std :: cout;
+using std :: swap;
+using std :: max;
+
+class MaxHeap{
+    public:
+    int capacity;
+    int * array;
+    MaxHeap(int capacity);
+    MaxHeap(int capacity,int * array);
+    const int parent(const int index);
+    const int right_child(const int index);
+    const int left_child(const int index);
+    void sift_up(int index);
+    void sift_down(int index);
+    void insert(int value,int index);
+    void extractMax();
+    void changePriority(int index , int value);
+    int getMax();
+    void remove(int index);
+    void print();
+    const int find(const int index , const int value);
+
+    
+
+};
