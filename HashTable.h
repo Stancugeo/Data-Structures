7abf7b2ba0103039d02d676edfc3e8c7a399bb40
Stancugeo <96280343+Stancugeo@users.noreply.github.com>
2022-08-17 06:13:04 -0700

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -1,11 +1,15 @@
-#include "SinglyLinkedList.h"
+#include "SinglyLinkedList.hpp"
+// Separate Chaining // 
 class HashTable{
     public:
     LinkedList * Table ;
     int array_size;
     HashTable(int size);
-    void AddinHash(int value);
-    void DeleteinHash(int value);
-    void Print();
+    void AddinTable(int value);
+    void DeleteinTable(int value);
+    void PrintTable();
     int HashFunction(int value);
+    bool IsEmpty(int index);
+    const int SearchItem(int value);
+    ~HashTable();
 };
