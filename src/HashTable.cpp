862c847063c3df89f43dfdabb14f08d7b5e2dc33
Stancugeo <96280343+Stancugeo@users.noreply.github.com>
2022-08-17 06:09:37 -0700

    Update HashTable.cpp

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
-#include "SinglyLinkedList.h"
-#include "HashTable1.cpp"
-
-
+#include "SinglyLinkedList.hpp"
 using namespace std;
-HashTable :: HashTable(int size){
+HashTable :: HashTable(int size){   
     this->Table = new LinkedList[size];
     this->array_size = size;
 }
-HashTable :: void AddinHash(int value){
+void HashTable ::  AddinTable(int value){
     int index = HashFunction(value);
     Table[index].Add(value);
 }
-HashTable :: void DeleteinHash(int value){
-    int index = HashFunction(value);
-    Table[index].Remove(value);
+void HashTable ::  DeleteinTable(int value){
+     int index = HashFunction(value);
+     Table[index].Remove(value);
 }
-HashTable :: void Print(){
-    for(int i = 0 ; i < array_size; i ++){
+void HashTable :: PrintTable(){
+     for(int i = 0 ; i < array_size; i ++){
         cout << i << ":";
         Table[i].Print();
         cout << endl;
     }
 }
-HashTable :: int HashFunction(int value){
+int HashTable :: HashFunction(int value){
     return value % array_size;
 }
-
+bool HashTable ::  IsEmpty(int index){
+    return (Table[index].IsEmpty());
+}
+HashTable :: const int SearchItem(int value){
+    if(Table[HashFunction(value)].Search(value)!=NULL) return value;
+    return -1;
+}
+~HashTable(){
+    delete[] Table;
+    Table = NULL;
+}
